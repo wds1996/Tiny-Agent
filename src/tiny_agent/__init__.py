abdb8a6bb26289df8114cd224155ed8a8c4b3ea2
Dosheng Wang <1005339793@qq.com>
2026-08-11 17:40:20 +0800

    chore: expose public runtime API

---
+++ src/tiny_agent/__init__.py
@@ -0,0 +1,13 @@
+from .runtime import AgentResult, AgentRuntime
+from .tool import Tool, ToolRegistry
+from .types import Model, ModelResponse, ToolCall
+
+__all__ = [
+    "AgentResult",
+    "AgentRuntime",
+    "Model",
+    "ModelResponse",
+    "Tool",
+    "ToolCall",
+    "ToolRegistry",
+]
