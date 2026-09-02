03fd8ac93519f5a0730b16c48c9bf39d99a600b3
Dosheng Wang <1005339793@qq.com>
2026-09-02 18:32:37 +0800

    feat(stage-11): export OpenScholar capstone primitives

---
+++ src/tiny_agent/capstone/__init__.py
@@ -0,0 +1,40 @@
+"""OpenScholar capstone: an integrated academic research Agent."""
+
+from .base_agent import BaseOpenScholarAgent, ResearchAgentConfig
+from .corpus import CorpusDocument, LocalResearchCorpus, extract_pdf_text, load_corpus_jsonl, write_corpus_jsonl
+from .evaluation import ResearchEvaluation, evaluate_research_report
+from .export import MarkdownReportExporter, render_report_markdown
+from .heuristic import HeuristicResearchModel
+from .memory import InMemoryResearchMemory, ResearchMemoryStore
+from .models import Critique, Evidence, ResearchMetrics, ResearchModel, ResearchPlan, ResearchReport, ResearchRequest
+from .openai_adapter import OpenAIResearchModel
+from .scholarly import CrossrefScholarlySearch, CrossrefSearchConfig, ScholarlySearchClient, StaticScholarlySearch
+
+__all__ = [
+    "BaseOpenScholarAgent",
+    "CorpusDocument",
+    "Critique",
+    "CrossrefScholarlySearch",
+    "CrossrefSearchConfig",
+    "Evidence",
+    "HeuristicResearchModel",
+    "InMemoryResearchMemory",
+    "LocalResearchCorpus",
+    "MarkdownReportExporter",
+    "OpenAIResearchModel",
+    "ResearchAgentConfig",
+    "ResearchEvaluation",
+    "ResearchMemoryStore",
+    "ResearchMetrics",
+    "ResearchModel",
+    "ResearchPlan",
+    "ResearchReport",
+    "ResearchRequest",
+    "ScholarlySearchClient",
+    "StaticScholarlySearch",
+    "evaluate_research_report",
+    "extract_pdf_text",
+    "load_corpus_jsonl",
+    "render_report_markdown",
+    "write_corpus_jsonl",
+]
